fb9f000731f3d08b18655ec52513b0887cd4d533
CuongNMs <nguyen.minh.cuong.1102@gmail.com>
2025-09-03 22:27:22 +0700

    init go prj

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/cuongnms/server-sent-event
+
+go 1.22.1
---
+++ main.go
@@ -0,0 +1,67 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"strconv"
+)
+
+func main() {
+	http.HandleFunc("/events", handleEvents)
+
+	log.Println("Server running at :8080")
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+func handleEvents(w http.ResponseWriter, r *http.Request) {
+	// Cấu hình SSE headers
+	count := r.URL.Query().Get("count")
+	if count == "" {
+		count = "1"
+	}
+	num, err := strconv.Atoi(count)
+	if err != nil {
+		fmt.Println("Error:", err)
+		return
+	}
+
+	w.Header().Set("Content-Type", "text/event-stream")
+	w.Header().Set("Cache-Control", "no-cache")
+	w.Header().Set("Connection", "keep-alive")
+
+	flusher, ok := w.(http.Flusher)
+	if !ok {
+		http.Error(w, "Streaming unsupported", http.StatusInternalServerError)
+		return
+	}
+
+	// Channel để gửi số random
+	msgChan := make(chan int)
+
+	// Goroutine sinh dữ liệu cho client này
+	go func() {
+		defer close(msgChan)
+
+		// random delay 1-5s
+		msgChan <- num + 2
+	}()
+
+	// Lắng nghe context để biết khi client disconnect
+	ctx := r.Context()
+
+	for {
+		select {
+		case <-ctx.Done():
+			log.Println("Client disconnected")
+			return
+		case num, ok := <-msgChan:
+			if !ok {
+				return // channel đóng => dừng
+			}
+			// Gửi event
+			fmt.Fprintf(w, "data: %d\nDone\n", num)
+			flusher.Flush()
+		}
+	}
+}
